92b1ed16aaeadcaab3fd14004d05a6ddf1d4a761
ohoohoo7 <63686427+ohoohoo7@users.noreply.github.com>
2021-12-06 10:43:21 +0900

    Update commax.py

--- commax-mqtt2elfin/pycommax/commax.py
+++ commax-mqtt2elfin/pycommax/commax.py
@@ -491,12 +491,12 @@ def do_work(config, device_list):
                             log('[SIGNAL] 신호 전송: {}'.format(send_data))
                         mqtt_client.publish(ELFIN_SEND_TOPIC, bytes.fromhex(send_data['sendcmd']))
                         # await asyncio.sleep(0.01)
-                        if send_data['count'] < 5:
+                        if send_data['count'] < 7:
                             send_data['count'] = send_data['count'] + 1
                             QUEUE.append(send_data)
                         else:
                             if elfin_log:
-                                log('[SIGNAL] Send over 5 times. Send Failure. Delete a queue: {}'.format(send_data))
+                                log('[SIGNAL] Send over 7 times. Send Failure. Delete a queue: {}'.format(send_data))
             except Exception as err:
                 log('[ERROR] send_to_elfin(): {}'.format(err))
                 return True
